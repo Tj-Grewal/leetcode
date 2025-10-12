7ddf1499ce92399145b42326914c77a6fd418592
TJ <tjgrewal.dev@gmail.com>
2025-10-12 16:44:34 -0700

    217 contains dupe

---
+++ lc-python/1-arrays/217-contains-duplicate.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) < len(nums)
+
+
+if __name__ == "__main__":
+    nums = [1, 2, 3, 1]
+    sol = Solution()
+    result = sol.containsDuplicate(nums)
+    print("Contains duplicate:", result)
