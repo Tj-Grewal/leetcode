85a79ecfb7096f803073202b5e8d7b4105d477cc
TJ <tjgrewal.dev@gmail.com>
2026-02-11 21:44:38 -0800

    128

--- lc-python/1-arrays/128-longest-consecutive-sequence.py
+++ lc-python/1-arrays/128-longest-consecutive-sequence.py
@@ -18,3 +18,24 @@ class Solution:
             orig = max(temp, orig)
 
         return orig
+    
+    
+# Better version - where you only use O(n) time and O(n) space
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        # Idea is to check if nums-1 exists in set, if not then this is start of sequence
+        # then just check if nums+1 exists while maintaining a length counter
+        numset = set(nums) # O(N) space complextity
+        longest = 0
+
+        for n in numset:
+            if (n-1) not in numset:
+                length = 0
+                while (n + length) in numset:
+                    length += 1
+                longest = max(length, longest)
+
+        return longest
