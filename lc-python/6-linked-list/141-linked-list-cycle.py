0551424a8f19d004c7147445f073ceddfa7302f9
TJ <tjgrewal.dev@gmail.com>
2026-02-14 16:44:45 -0800

    141

---
+++ lc-python/6-linked-list/141-linked-list-cycle.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        seen = set()
+        curr = head 
+
+        while curr:
+            if curr in seen:
+                return True
+            seen.add(curr)
+            curr = curr.next
+
+        return False
+    
+    # Floyd's Tortoise and Hare Algorithm
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+
+        return False
