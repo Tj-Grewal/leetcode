9a51a2107479ca5507512a75f9eea547e4e5afb9
TJ <tjgrewal.dev@gmail.com>
2026-02-14 19:04:57 -0800

    19

---
+++ lc-python/6-linked-list/19-remove-nth-node-from-end-of-list.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+from typing import Optional
+
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        if not head:
+            return
+
+        l = head
+        r = head
+
+        while n > 0:
+            r = r.next
+            n -= 1
+
+        if not r: # We ran out the end of the list
+            # this means either empty list or list of size 2
+            # If the list is [1], returning None is correct
+            # if the list is [1,2] returning 2 is correct
+            return head.next
+
+        prev = None
+        while r:
+            r = r.next
+            prev = l
+            l = l.next
+
+        temp = l.next
+        l = prev
+        l.next = temp
+        
+        return head
+    
+    
