4f7d295a075f8173f75ec7a06d174d657875d745
TJ <tjgrewal.dev@gmail.com>
2026-03-03 15:00:14 -0800

    update time and space complexity

--- lc-python/10-backtracking/39-comination-sum.py
+++ lc-python/10-backtracking/39-comination-sum.py
@@ -1,6 +1,15 @@
 
 from ast import List
 
+"""
+Time Complexity: O(2^(t/m)) where t is the target value and m is the minimum value in candidates. 
+This is because in the worst case, we can have a combination of numbers that add up to the
+target value, and we can either include or exclude each number in the combination. 
+The number of combinations is proportional to 2^(t/m) because we can have at most t/m numbers in the combination.
+Space Complexity: O(t/m) because in the worst case, we can have a combination of numbers that add up to the target value, 
+and we need to store that combination in the call stack during the depth-first search (DFS) process. 
+The maximum depth of the call stack is proportional to t/m because we can have at most t/m numbers in the combination.
+"""
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
--- lc-python/10-backtracking/79-word-search.py
+++ lc-python/10-backtracking/79-word-search.py
@@ -1,3 +1,15 @@
+from types import List
+
+""" 
+Time complexity: O(m*4^n) where m is the number of cells in the board and n is the length of the word.
+This is because in the worst case, we may have to explore every cell in the board (m) and for each cell, 
+we may have to explore up to 4 directions (up, down, left, right) for each character in the word (n).
+Space complexity: O(n) where n is the length of the word. This is because in the worst case, 
+we may have to explore a path that is as long as the word itself, and we need to store that path in the 
+call stack during the depth-first search (DFS) process. 
+The maximum depth of the call stack is proportional to n because we can have at most n characters in the word.
+"""
+
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         ROWS, COLUMNS = len(board), len(board[0])
