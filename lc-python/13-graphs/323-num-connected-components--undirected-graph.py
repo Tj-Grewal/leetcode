533d0a93d160721e0d1b97addc51c9fbb161b2bc
TJ <tjgrewal.dev@gmail.com>
2026-03-04 19:43:48 -0800

    323

--- lc-python/13-graphs/323-num-connected-components--undirected-graph.py
+++ lc-python/13-graphs/323-num-connected-components--undirected-graph.py
@@ -87,3 +87,58 @@ class Solution:
             """
 
         return res
+
+# union find:
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        res = n
+
+        parent = [i for i in range(n)]
+
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+
+        for u,v in edges:
+            pu, pv = find(u), find(v)
+            if pu != pv:
+                parent[pu] = pv
+                res -= 1
+
+        return res
+    
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        par = [i for i in range(n)]
+        rank = [1] * n
+
+        def find(n1):
+            res = n1
+
+            while res != par[res]:
+                # optimization to skip if we can
+                par[res] = par[par[res]] # otherwise this line does nothing
+                res = par[res]
+
+            return res
+
+        def union(n1,n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2: 
+                return 0
+
+            if p1 > p2:
+                par[p2] = p1
+                rank[p1] += rank[p2]
+            else:
+                par[p1] = p2
+                rank[p2] += rank[p1]
+            return 1
+
+        res = n
+        for n1, n2 in edges:
+            res -= union(n1,n2)
+        
+        return res
